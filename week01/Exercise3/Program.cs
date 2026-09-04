417f60f7ba50afcfc2f9414c2fdada17b4488dbc
jcarnevali1 <jcarnevali1@byupathway.edu>
2026-09-04 12:25:41 -0500

    W01 Assignment: C# Programming Exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        int number = -1;
+
+        int guess = -1;
+
+        Console.Write("What is the magic number? ");
+        number = int.Parse(Console.ReadLine());
+        
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> number_list = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                number_list.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in number_list)
+        {
+            sum += num;
+        }
+
+        float average = ((float)sum) / number_list.Count;
+
+        int max = number_list[0];
+
+        foreach (int num in number_list)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        int min = number_list[0];
+
+        foreach (int num in number_list)
+        {
+            if (num > 0 && num < min)
+            {
+                min = num;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is {max}");
+        Console.WriteLine($"The smallest positive number is {min}");
     }
 }
