cef0bddd08e8e938ec41029cbccfa7bc1b7a177c
jcarnevali1 <jcarnevali1@byupathway.edu>
2026-09-18 12:10:03 -0500

    W03 Team Activity: Scripture Memorizer Design

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,26 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
